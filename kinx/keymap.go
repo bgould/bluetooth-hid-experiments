1aa97dcf9a8f66d530da984f6b0d8a7af99f8824
BCG <bgould@users.noreply.github.com>
2023-11-12 11:40:40 -0500

    adjusting keymap

--- kinx/keymap.go
+++ kinx/keymap.go
@@ -15,14 +15,14 @@ func Keymap() keyboard.Keymap {
 	return keyboard.Keymap([]keyboard.Layer{
 		kinx.Layer(
 			ESC, F1, F2, F3, F4, F5, F6, F7, F8 /*  */, F9, F10, F11, F12, MUTE, VOLD, VOLU, FN0, PROG,
-			EQL, N1, N2, N3, N4, N5 /*                       */, N6, N7, N8, N9, N0, MINS,
-			TAB, Q, W, E, R, T /*                            */, Y, U, I, O, P, BSLS,
-			LCTL, A, S, D, F, G /*                           */, H, J, K, L, SCLN, QUOT,
-			LSFT, Z, X, C, V, B /*                           */, N, M, COMM, DOT, SLSH, RSFT,
-			/**/ GRV, INS, LEFT, RGHT /*                     */, UP, DOWN, LBRC, RBRC,
-			/*                 */ ESC, LGUI /*         */, LALT, RCTL,
-			/*                      */ HOME /*         */, PGUP,
-			/*            */ BSPC, DEL, END /*         */, PGDN, ENT, SPC, /**/
+			EQL, N1, N2, N3, N4, N5 /*                                    */, N6, N7, N8, N9, N0, MINS,
+			TAB, Q, W, E, R, T /*                                              */, Y, U, I, O, P, BSLS,
+			LCTL, A, S, D, F, G /*                                          */, H, J, K, L, SCLN, QUOT,
+			LSFT, Z, X, C, V, B /*                                     */, N, M, COMM, DOT, SLSH, RSFT,
+			/**/ GRV, INS, LEFT, RGHT /*                                */, UP, DOWN, LBRC, RBRC,
+			/*                 */ ESC, LGUI /*                  */, LALT, RCTL,
+			/*                      */ HOME /*                  */, PGUP,
+			/*            */ BSPC, DEL, END /*                  */, PGDN, ENT, SPC, /**/
 		),
 		kinx.Layer(
 			____, ____, ____, ____, ____, ____, ____, ____, ____ /*  */, ____, ____, ____, ____, PSCR, SLCK, BRK, ____, ____,
